227aa8134b2fa7b073eadb2e99e7cdce2b5dabcc
Pablo-jesuss <pablo.programador26@gmail.com>
2024-02-23 09:59:50 -0300

    todoForm

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Seleção de elementos
-
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelBtn = document.querySelector("#cancel-edit-btn");
 // Funções
 
 // Eventos
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log("Enviou form");
+});
