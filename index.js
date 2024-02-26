fdff7979649d98fd8cad60ff455d71caabef9422
Pablo-jesuss <pablo.programador26@gmail.com>
2024-02-25 22:48:09 -0300

    eraseBtn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelBtn = document.querySelector("#cancel-edit-btn");
 const searchInput = document.querySelector("#search-input");
-const eraseBtn = document.querySelector("#erase-button");
+const eraseBtn = document.querySelector("#erase-btn");
 const filterBtn = document.querySelector("#filter-select");
 
 let oldInputValue;
